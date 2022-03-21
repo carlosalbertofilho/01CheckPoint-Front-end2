8dafe4e5aec2ac15a70ebf8b868cff1945c5ac9f
LucasGuimaraes07 <lucas.aureliano07@gmail.com>
2022-03-20 21:50:26 -0300

    Commit convert Data input, atualizacao footer, inserir unshift

--- main.js
+++ main.js
@@ -13,14 +13,14 @@ let posts = [
 
     title: 'In the Land of Leadale',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.TMFDOjaTxFyJTcX4cGC4nAAAAA%26pid%3DApi&f=1',
-    firstRun: '5 DE JANEIRO',
+    firstRun: '05/01/2022',
     description: 'Sabe Sword Art Online? Então: a garota Keina sofre um acidente, fica em estado vegetativo e acorda no VRMMORPG World of Leadale, na pele de sua personagem, Cayna, só que centenas de anos depois na timeline daquele mundo, e com três filhos!'
   },
   {
 
     title: 'ORIENT',
     image: 'https://somoskudasai.com/wp-content/uploads/2020/12/2STAxF5u_2x.jpg',
-    firstRun: '5 DE JANEIRO',
+    firstRun: '20/01/2022',
     description: 'Adaptação do mangá de mesmo nome da mesma autora de Magi, Shinobu Ohtaka, desta vez a mangaká troca os contos das 1001 Noites por uma versão exagerada e fantástica de um Japão feudal assolado por demônios.'
   }
 ];
@@ -40,6 +40,7 @@ function renderPosts( postsReference ){
 
 renderPosts(posts);
 
+
 window.onload = function() {
   addNewPostReference.addEventListener('click', function(event){
     event.preventDefault();
@@ -53,13 +54,19 @@ window.onload = function() {
   });
   sendPostReference.addEventListener('click', function(event){
     event.preventDefault();
+    
+    var dataInput = dateFirstRunReference.value;
+    
+    data = new Date(dataInput);
+    dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+
     let register = {
       title: titlePostReference.value,
       image: urlPostReference.value,
-      firstRun: dateFirstRunReference.value,
+      firstRun: dataFormatada,
       description: descriptionPostReference.value
     };
-    posts.push(register);
+    posts.unshift(register);
     containerReference.className="posts";
     formNewPostReference.className="hidden";
     console.log(posts);
